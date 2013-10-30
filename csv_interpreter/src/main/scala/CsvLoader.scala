477e0c0aff95fc186b6a3f359385f5d64c4c0f4e
Radu Mirescu <radu@neodonis.com>
2013-10-30 21:08:24 +0200

    reasigned to private val member exception

---
+++ csv_interpreter/src/main/scala/CsvLoader.scala
@@ -0,0 +1,15 @@
+/**
+ * Created by: Radu Mirescu (radu@neodonis.com)
+ * Date: 30.10.2013
+ * Time: 21:04
+ */
+class CsvLoader {
+  private val fileToInterpret : String
+
+  def CsvLoader(path : String) {
+      fileToInterpret = path
+  }
+}
+object CsvLoader {
+
+}
